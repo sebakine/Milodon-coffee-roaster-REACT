f9e364cb6fc5997cb273e59cc369086b762bc617
sebakine <sebakine.m@gmail.com>
2025-11-27 23:48:13 -0300

    Config deploy GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/Milodon-coffee-roaster-REACT/",
   plugins: [react()],
   build: {
     rollupOptions: {
