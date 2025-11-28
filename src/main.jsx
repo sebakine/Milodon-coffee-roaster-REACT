08b6902e43cd61ed2cd0cfba98d0fb543ae23c7b
sebakine <sebakine.m@gmail.com>
2025-11-28 00:25:06 -0300

    Fix pages React

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
+
+// Bootstrap y estilos globales
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './styles/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </HashRouter>
+  </React.StrictMode>
 )
