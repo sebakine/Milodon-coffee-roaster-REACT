a45a03e0a113f3159b5374cb9cb5f3247bec9e85
sebakine <sebakine.m@gmail.com>
2025-11-28 00:12:44 -0300

    Fix de pages

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Milodon-coffee-roaster-REACT/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', 'framer-motion'],
-          ui: ['@fortawesome/fontawesome-free', 'bootstrap']
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
-  }
 })
